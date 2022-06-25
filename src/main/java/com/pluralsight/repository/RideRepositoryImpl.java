49d590ea8a2121cdcaf4a186e28cefa27db267f4
randoelicor <Jose_Leonard_Rico@manulife.ca>
2022-06-25 21:12:22 +0800

    Added logic for create and read

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,8 +1,22 @@
 package com.pluralsight.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.pluralsight.repository.util.RideRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -10,13 +24,40 @@ import com.pluralsight.model.Ride;
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public Ride createRide(Ride ride) {
+//		jdbcTemplate.update("INSERT INTO ride (name, duration) VALUES (?, ?)", ride.getName(), ride.getDuration());
+
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+				PreparedStatement ps = connection.prepareStatement("INSERT INTO ride (name, duration) VALUES (?, ?)", new String[] {"id"});
+				ps.setString(1, ride.getName());
+				ps.setInt(2, ride.getDuration());
+				return ps;
+			}
+		}, keyHolder);
+
+		Number id = keyHolder.getKey();
+
+		return getRide(id.intValue());
+	}
+
+	public Ride getRide(Integer id) {
+		Ride ride = jdbcTemplate.queryForObject("SELECT * FROM ride WHERE id = ?", new RideRowMapper(), id);
+
+		return ride;
+	}
+
 	@Override
 	public List<Ride> getRides() {
-		Ride ride = new Ride();
-		ride.setName("Corner Canyon");
-		ride.setDuration(120);
-		List <Ride> rides = new ArrayList<>();
-		rides.add(ride);
+		// Returns an ArrayList
+		List<Ride> rides = jdbcTemplate.query("SELECT * FROM ride", new RideRowMapper());
+
 		return rides;
 	}
 	
--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -1,10 +1,15 @@
 package com.pluralsight.controller;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import com.pluralsight.model.Ride;
@@ -12,6 +17,20 @@ import com.pluralsight.model.Ride;
 import org.junit.Test;
 
 public class RestControllerTest {
+
+	@Test(timeout = 3000)
+	public void testCreateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+		Ride ride = new Ride();
+
+		ride.setName("Wonder Flight");
+		ride.setDuration(50);
+
+//		restTemplate.put("http://localhost:8080/ride_tracker/ride", ride);
+		ride = restTemplate.postForObject("http://localhost:8080/ride_tracker/ride", ride, Ride.class); // post returns an object
+
+		System.out.println("Ride: " + ride);
+	}
 
 	@Test(timeout=3000)
 	public void testGetRides() {
